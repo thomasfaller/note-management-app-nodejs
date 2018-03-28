6a355a87c3a7bf575e26e090103e6a320edcc549
Thomas Faller <thomasfaller@Thomass-MacBook-Pro-2.local>
2018-03-28 20:43:29 +0100

    erased originals

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const log = (item) => {
 const error = chalk.bold.red
 const warning = chalk.keyword('orange')
 const success = chalk.bold.green
+const NoteNotFound = () => { warning('Note not found.') }
 
 log(success('Starting app.js...'))
 
@@ -32,10 +33,18 @@ if (command === 'add') {
 } else if (command === 'list') {
   notes.getAll()
 } else if (command === 'read') {
-  notes.getNote(argv.title)
+  const note = notes.getNote(argv.title)
+  if (note) {
+    log(success('Note found'))
+    log('---')
+    log(`Title: ${argv.title}`)
+    log(`Body: ${argv.body}`)
+  } else {
+    log(NoteNotFound())
+  }
 } else if (command === 'remove') {
   const noteRemoved = notes.removeNote(argv.title)
-  const message = noteRemoved ? success('Note was removed') : warning('Note not found.')
+  const message = noteRemoved ? success('Note was removed') : NoteNotFound();
   log(message)
 } else {
   console.log(warning('Command not found. \n You can type :\n - "add" to add new notes \n - "list" to list all notes.'))
